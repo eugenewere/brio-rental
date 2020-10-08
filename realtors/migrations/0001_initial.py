8b95b325eff9a5274208afeeaa41777f2696534f
eugene were <eugene.were@yahoo.com>
2020-10-08 23:04:30 +0300

    first commit

---
+++ realtors/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Realtor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=200)),
+                ('photo', models.ImageField(upload_to='photos/%Y/%m/%d/')),
+                ('description', models.TextField(blank=True)),
+                ('phone', models.CharField(max_length=20)),
+                ('email', models.CharField(max_length=50)),
+                ('is_mvp', models.BooleanField(default=False)),
+                ('hire_date', models.DateTimeField(blank=True, default=datetime.datetime.now)),
+            ],
+        ),
+    ]
